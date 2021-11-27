69e1b5bf5c9b7eb69e44786983f752817fa87cc6
devonlin101 <devonlin101@gmail.com>
2021-11-27 10:50:38 -0500

    add bashdaily.sh file

---
+++ bashdaily.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update -y;
+sudo apt-get upgrade -y;
+sudo apt-get dist-upgrade -y;
+sudo ubuntu-drivers autoinstall;
+npm outdated -g; 
+npm update -g;
+sudo apt-get clean -y;
+sudo apt-get autoclean -y;
+sudo apt-get autoremove -y;
+df -h;
+free -m;
+logo-ls;
+neofetch
